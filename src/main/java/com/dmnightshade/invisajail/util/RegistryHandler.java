1fdd7990081b3e7c120f85884f37d0c6eb4b43b6
Darren M. Nightshade <darren@dmnightshade.com>
2020-07-20 03:04:33 -0700

    added ruby tools; hoe broken; being special

--- src/main/java/com/dmnightshade/invisajail/util/RegistryHandler.java
+++ src/main/java/com/dmnightshade/invisajail/util/RegistryHandler.java
@@ -4,12 +4,15 @@ import com.dmnightshade.invisajail.InvisaJail;
 import com.dmnightshade.invisajail.blocks.BlockItemBase;
 import com.dmnightshade.invisajail.blocks.RubyBlock;
 import com.dmnightshade.invisajail.items.ItemBase;
+import com.dmnightshade.invisajail.tools.InvisaIJailItemTier;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import static com.dmnightshade.invisajail.tools.InvisaIJailItemTier.*;
 
 public class RegistryHandler {
 
@@ -23,6 +26,23 @@ public class RegistryHandler {
 
     //Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    //Tools
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
+            new SwordItem(InvisaIJailItemTier.RUBY, 2,-2.4F, new Item.Properties().group(InvisaJail.TAB)));
+
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
+            new PickaxeItem(InvisaIJailItemTier.RUBY, 0,-2.8F, new Item.Properties().group(InvisaJail.TAB)));
+
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
+            new ShovelItem(InvisaIJailItemTier.RUBY, 0.5F,-3.0F, new Item.Properties().group(InvisaJail.TAB)));
+
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
+            new AxeItem(InvisaIJailItemTier.RUBY, 0.5F,-3.0F, new Item.Properties().group(InvisaJail.TAB)));
+
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
+            new HoeItem(InvisaIJailItemTier.RUBY,-3, new Item.Properties().group(InvisaJail.TAB)));
+
 
     //Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
