a50eee59f6610eabb01349b5749e3cfb508e0b38
Darren M. Nightshade <darren@dmnightshade.com>
2020-07-20 00:15:17 -0700

    added ruby block

--- src/main/java/com/dmnightshade/invisajail/util/RegistryHandler.java
+++ src/main/java/com/dmnightshade/invisajail/util/RegistryHandler.java
@@ -1,7 +1,10 @@
 package com.dmnightshade.invisajail.util;
 
 import com.dmnightshade.invisajail.InvisaJail;
+import com.dmnightshade.invisajail.blocks.BlockItemBase;
+import com.dmnightshade.invisajail.blocks.RubyBlock;
 import com.dmnightshade.invisajail.items.ItemBase;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -11,12 +14,20 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class RegistryHandler {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, InvisaJail.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, InvisaJail.MOD_ID);
 
     public static void init(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     //Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+    //Blocks
+    public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
+
+    //Block Items
+    public static final RegistryObject<Item> RUBY_BLOCK_ITEM = ITEMS.register("ruby_block", () -> new BlockItemBase(RUBY_BLOCK.get()));
 
 }
